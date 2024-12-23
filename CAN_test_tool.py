47c3a65e2ae6040d58d0f87c5123aded834358dc
Arthur <Arthur@meanwell.com>
2024-12-23 11:34:31 +0800

    deleted no use files

--- CAN_test_tool.py
+++
@@ -1,9 +0,0 @@
-import can
-
-bus = can.interface.Bus(channel='can0', bustype='socketcan')
-
-print("Listenning")
-while True:
-	message = bus.recv()
-	if message:
-		print(f"Received message : {message}")
