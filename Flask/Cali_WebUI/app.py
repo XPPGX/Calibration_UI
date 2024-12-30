c805f9081d8e846de231c0113eae862b57dda9c6
XPPGX <qwerlegendary@gmail.com>
2024-12-30 02:49:04 +0000

    UI can be update while calibration not start yet

--- Flask/Cali_WebUI/app.py
+++ Flask/Cali_WebUI/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
+from datetime import datetime
 import time
 import ctypes
 from threading import Thread
 from collections import defaultdict
 
-########### Global Variables ############
+##################################################
+# Global Variables
+##################################################
 UI_Init_Flag = 0
 
 ModelName_ptr = ctypes.c_char_p
@@ -54,119 +57,158 @@ cali_status_cases = defaultdict(lambda: "Running...",  ## Suitable for a large n
         2 : "FAIL",
     }
 )
-#########################################
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 c_lib_Cali = ctypes.CDLL('./Clib/Cali.so')
 
+##################################################
+# C_lib parameters declaration
+##################################################
+
+c_lib_Cali.Get_Machine_Name.argtypes            = []
 c_lib_Cali.Get_Machine_Name.restype             = ctypes.c_char_p
+
+c_lib_Cali.Get_Communication_Type.argtypes      = []
 c_lib_Cali.Get_Communication_Type.restype       = ctypes.c_uint8
+
+c_lib_Cali.Get_Keyboard_Adjustment.argtypes     = []
 c_lib_Cali.Get_Keyboard_Adjustment.restype      = ctypes.c_uint8
+
+c_lib_Cali.Get_PSU_Calibration_Type.argtypes    = []
 c_lib_Cali.Get_PSU_Calibration_Type.restype     = ctypes.c_uint8
+
+c_lib_Cali.Get_Calibration_Status.argtypes      = []
 c_lib_Cali.Get_Calibration_Status.restype       = ctypes.c_uint8
+
+c_lib_Cali.Get_Calibration_Type_Point.argtypes  = []
 c_lib_Cali.Get_Calibration_Type_Point.restype   = ctypes.c_uint8
 
-ModelName_ptr = c_lib_Cali.Get_Machine_Name()
+c_lib_Cali.Start_Cali_thread.argtypes           = []
+c_lib_Cali.Start_Cali_thread.restype            = None
 
+##################################################
+# Functions implementation
+##################################################
 def server_timers():
     global UI_Init_Flag
+    
+    ModelName_ptr = c_lib_Cali.Get_Machine_Name()
 
     while True:
+        # socketio.emit('update', {'socket_CaliStatus' : f'update at {time.time()}'})
         if(UI_Init_Flag == 0):
             #Get ModelName string
             ModelName_str = ModelName_ptr.decode("utf-8")
-
+            if(ModelName_str == ""):
+                ModelName_str = "-"
+                print(f"ModelName = {ModelName_str}")
+            else:
+                print("received modelname from C : " + ModelName_str)
+            
             #Get CommInterface string
             CommInterface_uint8 = c_lib_Cali.Get_Communication_Type()
             CommInterface_str = comm_type_cases.get(CommInterface_uint8, "-") #"-" is default value
+            print(f'CommInterface_str = {CommInterface_str}')
 
             #Get AdjustMode
             AdjustMode_uint8 = c_lib_Cali.Get_Keyboard_Adjustment()
             AdjustMode_str = adjust_mode_cases[AdjustMode_uint8]
+            print(f'AdjustMode_str = {AdjustMode_str}')
 
             #Get CaliStatus
             CaliStatus_uint8 = c_lib_Cali.Get_Calibration_Status()
-            CaliStatus_str = cali_status_cases[CaliStatus_uint8]
 
-            socketio.emit('update', {
-                'socket_ModelName' : f'{ModelName_str}',
-                'socket_CommInterface' : f'{CommInterface_str}',
-                'socket_AdjustMode' : f'{AdjustMode_str}',
-                'socket_CaliStatus' : f'{CaliStatus_str}'
+            CaliStatus_str = cali_status_cases[CaliStatus_uint8]
+            print(f'CaliStatus_str = {CaliStatus_str}')
+
+            print("\n")
+            socketio.emit('update_1st_stage', {
+                'socket_ModelName' : f'{time.time()}, {ModelName_str}',
+                'socket_CommInterface' : f'{time.time()}, {CommInterface_str}',
+                'socket_AdjustMode' : f'{time.time()}, {AdjustMode_str}',
+                'socket_CaliStatus' : f'{time.time()}, {CaliStatus_str}'
             })
 
+            # UI_Init_Flag = 1
             if(CommInterface_str != "-"):
                 UI_Init_Flag = 1
+                print(UI_Init_Flag)
         else:
             #Get CaliType string
             CaliType_uint8 = c_lib_Cali.Get_PSU_Calibration_Type()
             CaliType_str = cali_type_cases.get(CaliType_uint8, "-")
+            print(CaliType_str)
 
             #Get CaliPoint string
             CaliPoint_uint8 = c_lib_Cali.Get_Calibration_Type_Point()
             CaliPoint_str = str(CaliPoint_uint8)
+            print(CaliPoint_str)
 
             #Get AdjustMode
             AdjustMode_uint8 = c_lib_Cali.Get_Keyboard_Adjustment()
             AdjustMode_str = adjust_mode_cases[AdjustMode_uint8]
+            print(AdjustMode_str)
 
             #Get CaliStatus
             CaliStatus_uint8 = c_lib_Cali.Get_Calibration_Status()
             CaliStatus_str = cali_status_cases[CaliStatus_uint8]
+            print(CaliStatus_str)
 
-            socketio.emit('update', {
-                'socket_CaliType' : f'{CaliType_str}',
-                'socket_CaliPoint' : f'{CaliPoint_str}',
-                'socket_AdjustMode' : f'{AdjustMode_str}',
-                'socket_CaliStatus' : f'{CaliStatus_str}'
+            socketio.emit('update_2nd_stage', {
+                'socket_CaliType' : f'{time.time()}, {CaliType_str}',
+                'socket_CaliPoint' : f'{time.time()}, {CaliPoint_str}',
+                'socket_AdjustMode' : f'{time.time()}, {AdjustMode_str}',
+                'socket_CaliStatus' : f'{time.time()}, {CaliStatus_str}'
             })
 
         time.sleep(0.1)
 
-
+##################################################
+# Web page routes
+##################################################
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+##################################################
+# Socket services
+##################################################
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
 
 @socketio.on('ui_start_cali')
 def handle_ui_start_cali():
-    UI_Init_Flag = 0
+#     UI_Init_Flag = 0
     
-    ModelName_ptr = c_lib_Cali.Get_Machine_Name()
-    ModelName_str = "-"
+#     ModelName_ptr = c_lib_Cali.Get_Machine_Name()
+#     ModelName_str = "-"
 
-    CommInterface_uint8 = 0
-    CommInterface_str   = "-"
+#     CommInterface_uint8 = 0
+#     CommInterface_str   = "-"
 
-    CaliType_uint8      = 0
-    CaliType_str        = "-"
+#     CaliType_uint8      = 0
+#     CaliType_str        = "-"
 
-    CaliPoint_uint8     = 0
-    CaliPoint_str       = "-"
+#     CaliPoint_uint8     = 0
+#     CaliPoint_str       = "-"
 
-    AdjustMode_uint8    = 0
-    AdjustMode_str      = "微調"
+#     AdjustMode_uint8    = 0
+#     AdjustMode_str      = "微調"
 
-    CaliStatus_uint8    = 0
-    CaliStatus_str      = "Running..."
+#     CaliStatus_uint8    = 0
+#     CaliStatus_str      = "Running..."
 
     thread = Thread(target=server_timers)
     thread.daemon = True
     thread.start()
+    
+    c_lib_Cali.Start_Cali_thread()
 
-    socketio.emit('WebHandler_init_ui', {
-        'socket_ModelName' : f'{ModelName_str}',
-        'socket_CommInterface' : f'{CommInterface_str}',
-        'socket_CaliType' : f'{CaliType_str}',
-        'socket_CaliPoint' : f'{CaliPoint_str}',
-        'socket_AdjustMode' : f'{AdjustMode_str}',
-        'socket_CaliStatus' : f'{CaliStatus_str}'
-    })
-
+##################################################
+# main
+##################################################
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- Flask/Cali_WebUI/static/js/index_script.js
+++ Flask/Cali_WebUI/static/js/index_script.js
@@ -1,15 +1,3 @@
-// document.getElementById('UI_btnStartSetting').addEventListener('click', async function() {
-//   const response = await fetch('/api/call',{
-//     method: 'POST',
-//     headers: {'Content-Type': 'application/json'},
-//     body:JSON.stringify({})
-//   });
-
-//   const data = await response.json();
-
-//   document.getElementById('result').innerText = `C Function Result: ${data.result}`;
-// })
-
 const socket = io.connect('http://localhost:5000')
 
 document.addEventListener("keydown", function(event){
@@ -26,17 +14,18 @@ document.getElementById('UI_btnStartSetting').onclick = function(){
     socket.emit('ui_start_cali');
 }
 
-socket.on('WebHandler_init_ui', function(data){
-    document.getElementById('UI_ModelName').innerText     = data.socket_ModelName;
+socket.on('update_1st_stage', function(data){
+    console.log(data);
+    document.getElementById('UI_ModelName').innerText = data.socket_ModelName;
     document.getElementById('UI_CommInterface').innerText = data.socket_CommInterface;
+    document.getElementById('UI_AdjustMode').innerText = data.socket_AdjustMode
+    document.getElementById('UI_Result').innerText = data.socket_CaliStatus;
+});
+
+socket.on('update_2nd_stage', function(data){
+    console.log(data);
     document.getElementById('UI_CaliType').innerText      = data.socket_CaliType;
     document.getElementById('UI_Cali_point').innerText    = data.socket_CaliPoint;
     document.getElementById('UI_AdjustMode').innerText    = data.socket_AdjustMode;
     document.getElementById('UI_Result').innerText        = data.socket_CaliStatus;
 })
-
-// socket.on('update', function(data){
-//   document.getElementById('UI_ModelName').innerText = data.socket_ModelName;
-    
-//   document.getElementById('result').innerText = data.message;
-// });
