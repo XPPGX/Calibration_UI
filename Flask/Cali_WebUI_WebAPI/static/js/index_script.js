7fc1a36de18e81c725f72cf0193092b38d96d992
XPPGX <qwerlegendary@gmail.com>
2025-01-06 03:43:53 +0000

    [Add] adjustmode color

--- Flask/Cali_WebUI_WebAPI/static/js/index_script.js
+++ Flask/Cali_WebUI_WebAPI/static/js/index_script.js
@@ -14,7 +14,14 @@ var Cali_Flag = 0;
  */
 var UI_stage = 1;
 
+
+var UI_AdjustMode_new_style = 'big_wood_style';
+
 var UI_Result_color = '';
+var UI_Result_color_map = {
+    "FINISH" : 'green_style',
+    "FAIL" : "red_style"
+};
 
 document.addEventListener("keydown", function(event){
     if(event.key === ' '){ //key == SPACE
@@ -34,13 +41,17 @@ document.getElementById('UI_btnStartSetting').addEventListener('click', async fu
             document.getElementById('UI_CommInterface').innerText = "-";
             document.getElementById('UI_CaliType').innerText = "-";
             document.getElementById('UI_Cali_point').innerText = "-";
-            document.getElementById('UI_AdjustMode').innerText = "微調";
-            document.getElementById('UI_Result').innerText = "-";
 
+            document.getElementById('UI_AdjustMode').innerText = "微調";
+            if(document.getElementById('UI_AdjustMode').classList.contains(UI_AdjustMode_new_style)){
+                document.getElementById('UI_AdjustMode').classList.remove(UI_AdjustMode_new_style);
+            }
+
+            document.getElementById('UI_Result').innerText = "-";
             if(document.getElementById('UI_Result').classList.contains(UI_Result_color)){
                 document.getElementById('UI_Result').classList.remove(UI_Result_color);
             }
-            
+
         case 0:
             Cali_Flag = 1;
             UI_stage = 1;
@@ -67,7 +78,7 @@ async function update_ui_stage(){
                     throw new Error("1st network response was not ok");
                 }
                 const res1_data = await response_1.json();
-
+                    
                 document.getElementById('UI_ModelName').innerText       = res1_data.WebAPI_ModelName;
                 document.getElementById('UI_CommInterface').innerText   = res1_data.WebAPI_CommInterface;
                 document.getElementById('UI_AdjustMode').innerText      = res1_data.WebAPI_AdjustMode;
@@ -92,31 +103,45 @@ async function update_ui_stage(){
                 document.getElementById('UI_AdjustMode').innerText      = res2_data.WebAPI_AdjustMode;
                 document.getElementById('UI_Result').innerText          = res2_data.WebAPI_CaliStatus;
                 
-                // if(document.getElementById('UI_Result').innerText == "FINISH" 
-                // || document.getElementById('UI_Result').innerText == "FAIL"){
+                if(document.getElementById('UI_AdjustMode').innerText == "粗調"){
+                    document.getElementById('UI_AdjustMode').classList.add(UI_AdjustMode_new_style);
+                }
+                else if(document.getElementById('UI_AdjustMode').innerText == "微調"){
+                    if(document.getElementById('UI_AdjustMode').classList.contains(UI_AdjustMode_new_style)){
+                        document.getElementById('UI_AdjustMode').classList.remove(UI_AdjustMode_new_style);
+                    }
+                }
+
+                
+                if(document.getElementById('UI_Result').innerText == "FINISH" 
+                || document.getElementById('UI_Result').innerText == "FAIL"){
+                    UI_Result_color = UI_Result_color_map[document.getElementById('UI_Result').innerText];
+                    document.getElementById('UI_Result').classList.add(UI_Result_color);
+                    setTimeout(() => {
+                        UI_stage = 3;
+                        Cali_Flag = 2;
+                    });
+                }
+                
+
+
+                // if(document.getElementById('UI_Result').innerText == "FINISH"){
+                //     UI_Result_color = 'green_style';
+                //     document.getElementById('UI_Result').classList.add(UI_Result_color);
+                //     setTimeout(() => {
+                //         UI_stage = 3;
+                //         Cali_Flag = 2;
+                //     });
+
+                // }
+                // else if(document.getElementById('UI_Result').innerText == "FAIL"){
+                //     UI_Result_color = 'red_style'
+                //     document.getElementById('UI_Result').classList.add(UI_Result_color);
                 //     setTimeout(() => {
                 //         UI_stage = 3;
                 //         Cali_Flag = 2;
                 //     });
                 // }
-
-                if(document.getElementById('UI_Result').innerText == "FINISH"){
-                    UI_Result_color = 'green_style';
-                    document.getElementById('UI_Result').classList.add(UI_Result_color);
-                    setTimeout(() => {
-                        UI_stage = 3;
-                        Cali_Flag = 2;
-                    });
-
-                }
-                else if(document.getElementById('UI_Result').innerText == "FAIL"){
-                    UI_Result_color = 'red_style'
-                    document.getElementById('UI_Result').classList.add(UI_Result_color);
-                    setTimeout(() => {
-                        UI_stage = 3;
-                        Cali_Flag = 2;
-                    });
-                }
 
                 break;
                 
